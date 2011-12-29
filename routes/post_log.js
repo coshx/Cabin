6588f6fffaeb29927cbdd4350452a649da2243b4
Anthony J Burton <ant@coshx.com>
2011-12-29 16:19:46 -0500

    fix logic in save function

--- routes/post_log.js
+++ routes/post_log.js
@@ -4,10 +4,13 @@ module.exports = function(req, res){
   console.log(req.body);
   var log = new models.AppLog(req.body);
   log.save(function(err) {
-    console.log("ZOMG HORRIBLE ERROR TRYING TO SAVE LOG:", err);
-    console.log("Body was:", req.body);
-    console.log("but that wasn't good enough");
-    res.send("Invalid log", 406);
+    if (err) {
+      console.log("ZOMG HORRIBLE ERROR TRYING TO SAVE LOG:", err);
+      console.log("Body was:", req.body);
+      console.log("but that wasn't good enough");
+      res.send("Invalid log", 406);
+    } else {
+      res.send("OK", 201);
+    }
   });
-  res.send("OK", 201);
 };
